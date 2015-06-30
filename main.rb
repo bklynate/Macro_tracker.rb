8db36cdd73acb8fc443f910433916b68d03f26fa
bklynate <nathanielcfa@gmail.com>
2015-06-30 00:03:02 -0400

    initial commit

--- main.rb
+++ main.rb
@@ -24,6 +24,18 @@ post '/set_username' do
 end
 
 get '/set_macros' do
+  erb :set_macros
+end
+
+post '/set_macros' do
+  session[:protein_macros] = params[:protein_macros].to_i
+  session[:fat_macros] = params[:fat_macros].to_i
+  session[:carb_macros] = params[:carb_macros].to_i
+
+  if session[:protein_macros] < 0 || session[:fat_macros] < 0 || session[:carb_macros] < 0
+    @error = "Invalid Input"
+    halt erb(:set_macros)
+  end
 
   erb :set_macros
 end
