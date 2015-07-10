e8eec65931d5795fe6bf3e4c1cac4cb823213e4a
bklynate <nathanielcfa@gmail.com>
2015-07-10 07:19:09 -0400

    working progress

--- main.rb
+++ main.rb
@@ -7,20 +7,30 @@ use Rack::Session::Cookie, :key => 'rack.session',
                            :secret => '19874310BKLYN'
 
 get '/' do  
-  if session[:username].empty?
-    redirect '/set_username'
-  elsif session[:username]
+  if session[:username]
     redirect '/set_macros'
+  else
+    redirect '/set_username'
   end
 end
 
 get '/set_username' do
-
   erb :set_username
 end
 
 post '/set_username' do
-
+  if session[:username].empty?
+    @error = "<strong>Please provide your name..</strong>"
+  elsif session[:username].to_i > 0
+    @error = "<strong>No numbers allowed in this field</strong>"
+  elsif session[:username].length > 13
+    @error = "<strong>Too many characters..</strong>"
+  else
+    session[:username] = params[:username].capitalize
+    redirect '/set_macros'
+  end
+  
+  erb :set_username
 end
 
 get '/set_macros' do
@@ -34,8 +44,13 @@ post '/set_macros' do
 
   if session[:protein_macros] < 0 || session[:fat_macros] < 0 || session[:carb_macros] < 0
     @error = "Invalid Input"
-    halt erb(:set_macros)
+  elsif session[:protein_macros] && session[:fat_macros] && session[:carb_macros] 
+    redirect '/display_macros'
   end
 
   erb :set_macros
+end
+
+get '/display_macros' do
+  erb :display_macros
 end
